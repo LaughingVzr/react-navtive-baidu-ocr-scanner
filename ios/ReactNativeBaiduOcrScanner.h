63373251c15e502c0b131753900d2a97b5023b7a
Laughing_Vzr <LouWazor@gmail.com>
2019-02-19 17:37:55 +0800

    feat:增加ios扫描接口

--- ios/ReactNativeBaiduOcrScanner.h
+++ ios/ReactNativeBaiduOcrScanner.h
@@ -5,7 +5,13 @@
 #import <React/RCTBridgeModule.h>
 #endif
 
-@interface ReactNativeBaiduOcrScanner : NSObject <RCTBridgeModule>
+#import <React/RCTEventEmitter.h>
+#import <AipOcrSdk/AipOcrSdk.h>
+#import <Foundation/Foundation.h>
 
+@interface ReactNativeBaiduOcrScanner :RCTEventEmitter <RCTBridgeModule, UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+
+@property (nonatomic, strong) RCTPromiseResolveBlock resolve;
+@property (nonatomic, strong) RCTPromiseRejectBlock reject;
 @end
   
--- ios/ReactNativeBaiduOcrScanner.m
+++ ios/ReactNativeBaiduOcrScanner.m
@@ -2,12 +2,73 @@
 #import "ReactNativeBaiduOcrScanner.h"
 
 @implementation ReactNativeBaiduOcrScanner
-
+@synthesize bridge = _bridge;
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
 
+
+-(instancetype) init{
+    self = [super init];
+    
+    //添加监听
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getMessageContent:) name:@"Scan_Send_Message" object:nil];
+    
+    return self;
+}
+
+- (UIViewController*) getRootVC {
+    UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    while (root.presentedViewController != nil) {
+        root = root.presentedViewController;
+    }
+    return root;
+}
+
+//初始化
+RCT_EXPORT_METHOD(initOcr:(NSString*)ak  sk:(NSString*)sk  callback:(RCTResponseSenderBlock)callback)
+{
+    NSLog(@"ak=%@ sk=%@", ak, sk);
+    [[AipOcrService shardService] authWithAK:ak andSK:sk];
+    callback(@[[NSNumber numberWithInt:0]]);
+}
+
+-(void) IDCardFrontScanner{
+    UIViewController * vc =
+    [AipCaptureCardVC ViewControllerWithCardType:CardTypeLocalIdCardFont andImageHandler:^(UIImage *image) {
+        // 成功扫描出身份证
+        [[AipOcrService shardService] detectIdCardFrontFromImage:image
+                                                     withOptions:nil
+                                                  successHandler:^(id result){
+                                                      // 在成功回调中，保存图片到系统相册
+                                                      UIImageWriteToSavedPhotosAlbum(image, nil, nil, (__bridge void *)self);
+                                                      // 打印出识别结果
+                                                      NSLog(@"%@", result);
+                                                  }
+                                                     failHandler:_failHandler];
+    }];
+    // 展示ViewController
+    [[self getRootVC] presentViewController: vc animated:YES completion:nil];
+}
+
+-(void) IDCardBackScanner{
+    UIViewController * vc =
+    [AipCaptureCardVC ViewControllerWithCardType:CardTypeLocalIdCardFont andImageHandler:^(UIImage *image) {
+        // 成功扫描出身份证
+        [[AipOcrService shardService] detectIdCardBackFromImage:image
+                                                     withOptions:nil
+                                                  successHandler:^(id result){
+                                                      // 在成功回调中，保存图片到系统相册
+                                                      UIImageWriteToSavedPhotosAlbum(image, nil, nil, (__bridge void *)self);
+                                                      // 打印出识别结果
+                                                      NSLog(@"%@", result);
+                                                  }
+                                                     failHandler:_failHandler];
+    }];
+    // 展示ViewController
+    [[self getRootVC] presentViewController: vc animated:YES completion:nil];
+}
 @end
   
